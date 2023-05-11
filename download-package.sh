05d8028a89830409e245ca18e36ebcedb35c9c60
Aoang <aoang@x2oe.com>
2023-05-11 17:01:12 +0800

    fix url

--- download-package.sh
+++ download-package.sh
@@ -5,7 +5,7 @@ arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 # ${DOWNLOAD_URL} == https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${VERSION}
 URL="${DOWNLOAD_URL}_${arch}.deb/download.deb"
 
-if [[ ${uname -m} == "amd64" ]]; then
+if [[ ${arch} == "amd64" ]]; then
   URL=${URL/focal/bionic}
 fi
 

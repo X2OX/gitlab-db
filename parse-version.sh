694be80ef63be40df9f0c75d82c4281c5603f936
Aoang <aoang@x2oe.com>
2023-05-11 18:15:24 +0800

    test

--- parse-version.sh
+++ parse-version.sh
@@ -8,6 +8,7 @@ fi
 
 for i in $(seq 1 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/gitlab/gitlab-ee?page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | grep -Eo '(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)' >> version.list;
+    echo "$(curl -s "https://packages.gitlab.com/gitlab/gitlab-ee?page=${i}" | grep "_arm64.deb" | grep -v '\-rc')"
     cat version.list
 done
 

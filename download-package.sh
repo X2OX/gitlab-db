883a8dcb9c144e2f5a07d4bd4133e9319e96ce9f
Aoang <aoang@x2oe.com>
2023-05-11 16:40:36 +0800

    fix get arch

--- download-package.sh
+++ download-package.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Ubuntu `x86_64:amd64 aarch64:arm64`
-arch="amd64"
-if [[ ${uname -m} == "aarch64" ]]; then
-  arch="arm64"
-fi
+arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
+echo "$arch"
 
 # ${DOWNLOAD_URL} == https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${VERSION}
 URL="${DOWNLOAD_URL}_${arch}.deb/download.deb"
 
 echo "Downloading package - ${URL}"
-wget --quiet ${URL} -O /tmp/gitlab.deb
+wget --quiet ${URL} -O gitlab.deb
 
 results=$?
 if [ ${results} -ne 0 ]; then

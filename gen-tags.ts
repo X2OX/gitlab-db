010b747f728931b54ce24a7004d896b2c8e00feb
Aoang <aoang@x2oe.com>
2023-05-12 11:15:00 +0800

    fix

--- gen-tags.ts
+++ gen-tags.ts
@@ -2,9 +2,10 @@ import { isLatest } from "./dockerhub.ts";
 import Version from "./version.ts";
 
 const ref = Deno.env.get("GITHUB_REF")?.slice(11) as string;
-console.log(`ghcr.io/x2ox/gitlab-db:v${ref}`);
+let output = `tags=ghcr.io/x2ox/gitlab-db:v${ref}`
 
 const ok = await isLatest(new Version(ref));
 if (ok) {
-  console.log(`ghcr.io/x2ox/gitlab-db:latest`);
+  output+=`,ghcr.io/x2ox/gitlab-db:latest`
 }
+console.log(output)

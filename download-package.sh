b44cddd183d76eb5f91e4a28e1cf93587d71fd0e
Aoang <aoang@x2oe.com>
2024-05-17 09:58:17 +0800

    Fix v17 Download

--- download-package.sh
+++ download-package.sh
@@ -4,10 +4,6 @@ arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
 
 URL="${DOWNLOAD_URL}_${arch}.deb/download.deb"
 
-if [[ "$DOWNLOAD_URL" == *".deb" ]]; then
-  URL="${DOWNLOAD_URL}"
-fi
-
 echo "Downloading package - ${URL}"
 wget --quiet ${URL} -O /tmp/gitlab.deb
 
